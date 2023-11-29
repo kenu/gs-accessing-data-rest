5fa431135688e566fee576c025d9a3b77fb430fb
hayoungBae <960819hy@gmail.com>
2023-11-29 16:37:18 +0900

    hayoung

---
+++ complete/gs-test/src/main/java/com/example/gstest/GsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsTestApplication.class, args);
+    }
+
+}
